6da10ca87249e0b4ba6d699103e769f8d58bff41
threeveisgood <whdgnl7479@naver.com>
2020-07-14 16:09:29 +0900

    2

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,27 +1,51 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Container, Grid, Fab, Button, TextField } from "@material-ui/core";
+import { useMutation, useLazyQuery, useQuery } from "@apollo/react-hooks";
+import  gql from 'graphql-tag'
 import { Link } from "next/link";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
 import Layout from "../../components/layout";
 
+const GET_POSTS = gql`
+  query Posts {
+    posts {
+      id
+      name
+      description
+      imgUrl
+    }
+  }
+`
 
 export default () => {
-  const [posts, setPosts] = useState([]);
+  const { loading, error, data } = useQuery(GET_POSTS)
   const [open, setOpen] = useState(false);
 
-  const { users } = data
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  //const { users } = data
 
   return (
     <Layout>
       <div>
         <h1>writePost</h1>
         <Grid container justify="flex-start" spacing={8}>
-        {posts.map((post, i) => (
+        {data.posts.map((post, i) => (
             <Grid item xs={12} md={4}>
-              <div key={i}>{post.name}</div>
+              <div key={i}>
+                <div>
+                {post.name}
+                </div>
+                <div>
+                {post.description}
+                </div>
+                <div>
+                {post.imgUrl}
+                </div>
+              </div>
             </Grid>
           ))}
         </Grid>
