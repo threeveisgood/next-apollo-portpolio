f94131029d7e068becf69f46646d7e185423cc75
threeveisgood <whdgnl7479@naver.com>
2020-09-15 14:36:22 +0900

    v9

--- components/addPost.js
+++ components/addPost.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { makeStyles } from '@material-ui/core/styles';
-
+import { makeStyles } from "@material-ui/core/styles";
+import Editor from './write/editor'
 import {
   Fab,
   Button,
@@ -19,13 +19,13 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContentText from "@material-ui/core/DialogContentText";
 
 const useStyles = makeStyles((theme) => ({
-    fab: {
-      background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-      position: "fixed",
-      right: 16,
-      bottom: 16,
-    }
-}))
+  fab: {
+    background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
+    position: "fixed",
+    right: 16,
+    bottom: 16,
+  },
+}));
 
 const FormWrapper = styled.div({
   display: "flex",
@@ -36,11 +36,12 @@ const FormWrapper = styled.div({
 });
 
 const ADD_POST = gql`
-  mutation AddPost($name: String!, $description: String!, $imgUrl: String!) {
-    addPost(name: $name, description: $description, imgUrl: $imgUrl) {
+  mutation AddPost($name: String!, $description: String!, $imgUrl: String!, $category: String!) {
+    addPost(name: $name, description: $description, imgUrl: $imgUrl, category: $category) {
       id
       name
       description
+      category
       imgUrl
     }
   }
@@ -48,7 +49,7 @@ const ADD_POST = gql`
 
 export default () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);  
+  const [open, setOpen] = useState(false);
   const [addPost] = useMutation(ADD_POST, {
     refetchQueries: ["Posts"],
     onCompleted: (result) => {
@@ -59,42 +60,56 @@ export default () => {
   const handleClose = () => setOpen(false);
 
   const handleAddPost = (values) => {
-    addPost({ variables: values  });
+    addPost({ variables: values });
   };
 
-  return (      
-      <> 
-       <Fab className={classes.fab} color="primary" aria-label="add" onClick={() => setOpen(true)}>
-         <AddIcon />
-       </Fab>
-       <Formik
+  return (
+    <>
+      <Fab
+        className={classes.fab}
+        color="primary"
+        aria-label="add"
+        onClick={() => setOpen(true)}
+      >
+        <AddIcon />
+      </Fab>
+      <Formik
         initialValues={{ name: "", price: "", imgUrl: "" }}
         validationSchema={Yup.object({
           name: Yup.string().required(),
           description: Yup.string().required(),
-          imgUrl: Yup.string().required()
+          imgUrl: Yup.string().required(),
+          category: Yup.string().required()
         })}
         onSubmit={(values) => {
-          console.log("onSubmit")
-          handleAddPost(values)
+          console.log("onSubmit");
+          handleAddPost(values);
         }}
-        >
-          {(props) => (
-            <form onSubmit={props.handleSubmit}>
-              <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="form-dialog-title"
-                maxWidth="sm"
-                fullWidth
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit}>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="form-dialog-title"
+              maxWidth="sm"
+              fullWidth
             >
               <DialogTitle id="form-dialog-title">Write Post</DialogTitle>
               <DialogContent>
-                <DialogContentText>This post is stored in MongoDB</DialogContentText>
+                <DialogContentText>
+                  This post is stored in MongoDB
+                </DialogContentText>
                 <FormWrapper>
                   <TextField
                     label="Post Name"
                     name="name"
+                    variant="outlined"
+                    onChange={props.handleChange}
+                  />
+                  <TextField
+                    label="Category"
+                    name="category"
                     variant="outlined"
                     onChange={props.handleChange}
                   />
@@ -112,7 +127,7 @@ export default () => {
                     name="imgUrl"
                     variant="outlined"
                     onChange={props.handleChange}
-                  />
+                  />                                 
                 </FormWrapper>
               </DialogContent>
               <DialogActions>
@@ -129,9 +144,9 @@ export default () => {
                 </Button>
               </DialogActions>
             </Dialog>
-            </form>
-          )}
-        </Formik>
-        </>
+          </form>
+        )}
+      </Formik>
+    </>
   );
 };
