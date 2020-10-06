6bdbb69b72fb781fc9cdc30acd372dfad1f222f2
threeveisgood <whdgnl7479@naver.com>
2020-10-07 00:22:27 +0900

    'v12'

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import {
-  Container,
-  Grid,
-  Typography,
-} from "@material-ui/core";
+import { Container, Grid, Typography } from "@material-ui/core";
+import Pagination from "@material-ui/lab/Pagination";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import styled from "styled-components";
 //import Reactmarkdown from 'react-markdown'
 
 import Layout from "../../components/layout";
+
+const PostGrid = styled(Grid)({
+  //  padding: '8px !important'
+});
 
 const GET_ARTICLES = gql`
   query Articles($start: Int!, $limit: Int!) {
@@ -28,41 +30,74 @@ const GET_ARTICLES = gql`
   }
 `;
 
+const GET_AGGREGATE = gql`
+  query ArticlesConnection {
+    articlesConnection {
+      aggregate {
+        count
+        totalCount
+     }
+  }
+}
+`;
+
 export default () => {
-   const router = useRouter();
+  const router = useRouter();
 
-   const postCount = 3;
-   const page = parseInt(router.query.page || '1', 10)
-   const start = (page - 1) * 3
-   const LastPage = Math.ceil(postCount / 3)
+  const postCount = 3;
+  const page = parseInt(router.query.page || "1", 10);
+  const start = (page - 1) * postCount;
 
-  const { loading, error, data } = useQuery(GET_ARTICLES,
-    { variables: { limit: postCount, start: start }}
-   );  
+  const { loading, error, data } = useQuery(GET_ARTICLES, {
+    variables: { limit: postCount, start: start },
+  });
 
-  console.log(page)
-
+  const { loading: loadingA, error: errorA, data: dataA } = useQuery(GET_AGGREGATE);
+  
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
 
+  console.log(data.articles)
+  console.log(dataA.articlesConnection.aggregate.count);
+
+  const LastPage = Math.ceil(dataA.articlesConnection.aggregate.count / 3);
+  
   return (
     <Layout>
       <Container maxWidth="sm">
-        <div>          
-          <Grid container justify="flex-start" spacing={8}>
+        <div>
+          <Grid
+            container
+            justify="flex-start"
+            spacing={1}
+            alignItems="center"
+            justify="center"
+          >
             {data.articles.map((article) => (
-                <>           
-                  <Grid item xs={12} md={12} id={article.id}>
+              <React.Fragment>
+                <PostGrid item xs={9} md={9}>
                   <Typography variant="subtitle2" gutterBottom>
-                      {article.title}
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom>
-                      {article.published_at}
-                    </Typography>
-                  </Grid>
-                </>
+                    {article.title}
+                  </Typography>
+                </PostGrid>
+                <PostGrid item xs={3} md={3}>
+                  {article.published_at.substring(5, 10).replace("-", "/")}
+                </PostGrid>
+              </React.Fragment>
             ))}
-          </Grid>          
+          </Grid>
+          <Grid
+            container
+            justify="flex-start"
+            spacing={1}
+            alignItems="center"
+            justify="center"
+            style={{ marginTop: "5vh" }}
+          >
+            <Grid item>
+              <Pagination count={LastPage} color="primary" />
+            </Grid>
+          </Grid>
         </div>
       </Container>
     </Layout>
@@ -75,14 +110,13 @@ export default () => {
 
 //   console.log(router.query.page)
 
-
 //   return (
 //     <Layout>
 //       <Container maxWidth="md">
-//         <div>          
+//         <div>
 //           <Grid container justify="flex-start" spacing={8}>
 //             {data.articles.map((article) => (
-//                 <>           
+//                 <>
 //                   <Grid item xs={12} md={12} id={article.id}>
 //                   <Typography variant="subtitle2" gutterBottom>
 //                       {article.title}
@@ -93,7 +127,7 @@ export default () => {
 //                   </Grid>
 //                 </>
 //             ))}
-//           </Grid>          
+//           </Grid>
 //         </div>
 //       </Container>
 //     </Layout>
@@ -102,13 +136,13 @@ export default () => {
 
 // export const getStaticPaths = async () => {
 
-//   const { loading, error, dataR } = useQuery(GET_ARTICLES);  
-  
+//   const { loading, error, dataR } = useQuery(GET_ARTICLES);
+
 //   if (loading) return "Loading...";
 //   if (error) return `Error! ${error.message}`;
 
-//   const paths = dataR.map(post => 
-//     {    
+//   const paths = dataR.map(post =>
+//     {
 //      params: { id: post.id }
 //     }
 //   )
@@ -120,6 +154,6 @@ export default () => {
 // }
 
 // export const getStaticProps = async () => {
-//   const { data } = useQuery(GET_ARTICLES);  
+//   const { data } = useQuery(GET_ARTICLES);
 //   return { props: { data } };
 // };
